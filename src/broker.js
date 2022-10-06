bfe124e079c16adc5a7d9f1a6a1e632144bb567c
Carlos MEDINA <Carlos.MEDINA@ingenico.com>
2022-10-06 16:44:56 -0500

    [CLEAN] configurando topicos

--- src/broker.js
+++ src/broker.js
@@ -1,111 +1,116 @@
 const mosca = require('mosca');
 const http = require('https');
 const mqtt = require('mqtt');
-// const fs = require("fs");
+const topics = require("../src/storage.json")
 
-//Configuracion MQTT
-const hostMQTT = 'mqtt://192.168.5.59:1883';
-const settings = {port: 1883}
+const topicRecord = 'record';
+const topicAdd = 'addProduct';
+const topicCharger = 'charger';
+const topicPerformance = 'performance';
+const topicQuality = 'qualityproduct';
+const ip = "192.168.5.221";
 
-topic1 = 'record';
-topic2 = 'add';
-topic3 = 'charger';
-topic4 = 'perform';
-topic5 = 'quality';
-
-//Configuracion HTTP
-const hostHTTP = '192.168.5.59';
-const portHTTP = 3000;
-//Conexion a la API https://4trz3v4f7f.execute-api.us-east-1.amazonaws.com/productive/handlerdata/recordsprocess
-const optionsHTTP = {
-    hostname: '4trz3v4f7f.execute-api.us-east-1.amazonaws.com', //'192.168.5.59',
-    port: 443, //3008,
-    path: '/productive/handlerdata/recordsprocess', //'/api/users',
+// AWS
+const optionsAWS = {
+    hostname: 'keb6atfcl0.execute-api.us-east-1.amazonaws.com',
+    port: 443,
+    // path: '/prueba/handlerdata/qualityproduct',
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     }
 }
 
-console.clear()
+function selectTopic(topic){
+    if (topics[topic]) {
+        optionsAWS.path = topics[topic];
+    } else {
+        console.log(`\n%c${'El topic es incorrecto'}`)
+    }
+    // switch(topic){
+    //     case topicRecord:
+    //         optionsAWS.path = '/prueba/handlerdata/recordsprocess';
+    //         break;
+    //     case topicAdd:
+    //         optionsAWS.path = '/prueba/handlerdata/addproducto';
+    //         break;
+    //     case topicCharger:
+    //         optionsAWS.path = '/prueba/handlerdata/chargerawmaterial';
+    //         break;
+    //     case topicPerformance:
+    //         optionsAWS.path = '/prueba/handlerdata/performance';
+    //         break;
+    //     case topicQuality:
+    //         optionsAWS.path = '/prueba/handlerdata/qualityproduct';
+    //         break;
+    //     default:
+    //         console.log(`\n%c${'El topic es incorrecto'}`)
+    // }
+}
+
+const reqAWS = () => {
+    return http.request(optionsAWS, res => {
+        console.log(`\nHTTP: Status: ${res.statusCode} ${res.statusMessage}`)
+        res.on('data', d => {
+            console.log(`\nAWS responde desde ${optionsAWS.path}`)
+            process.stdout.write(d)
+        })
+    })
+}
+
+// MQTT
+const settings = {port: 1234}
 const broker = new mosca.Server(settings)
 
-const client = mqtt.connect(hostMQTT);
-
-broker.on('published', (packet)=>{
+broker.on('published', (packet) => {
     message = packet.payload.toString();
     topic = packet.topic.toString();
 
     console.clear();
     console.log(`MQTT:Desde el topic '${topic}', se envia el mensaje\n${message}`);
 
-    switch(topic){
-        case topic1:
-            optionsHTTP.path = '/productive/handlerdata/recordsprocess';
-            break;
-        case topic2:
-            optionsHTTP.path = '/productive/handlerdata/addproducto';
-            break;
-        case topic3:
-            optionsHTTP.path = '/productive/handlerdata/chargerawmaterial';
-            break;
-        case topic4:
-            optionsHTTP.path = '/productive/handlerdata/performance';
-            break;
-        case topic5:
-            optionsHTTP.path = '/productive/handlerdata/qualityproduct';
-            break;
-        default:
-            console.log(`\n%c${'El topic es incorrecto'}`, `color:${'red'}`)
-    }
+    selectTopic(topic);
 })
 
-//HTTP server
+const hostMQTT = `mqtt://${ip}:1234`;
+const client = mqtt.connect(hostMQTT);
+
+function connectMQTT(){
+    client.on('connect', ()=>{
+        client.subscribe(topicRecord);
+        client.subscribe(topicAdd);
+        client.subscribe(topicCharger);
+        client.subscribe(topicPerformance);
+        client.subscribe(topicQuality);
+    })
+}
+
+function messageMQTT(){
+    client.on('message', (topic, message) => {
+        data = JSON.parse(message);
+        data.topic = topic;
+        
+        req = reqAWS();          
+        req.on('error', error => {
+            console.error(error)
+        })
+        req.write(JSON.stringify(data))
+        req.end()
+    })
+}
+
+//Servidor HTTP
+const hostHTTP = ip;
+const portHTTP = 3000;
+
 const server = http.createServer((req, res) => {
-    // console.log('Se ha accedido desde el navegador');
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola mundo')
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end('Hola mundo')
 })
 
 server.listen(portHTTP, hostHTTP, () => {
     console.log('Servidor funcionando en', hostHTTP, portHTTP);
-
-    client.on('message', (topic, message) => {
-        data = JSON.parse(message);
-        data.topic = topic;
-        // console.log(data)
-
-        // fs.writeFile('datos.txt', message, function(err){
-        //     if (!err){
-        //         console.log('Se ha persistido los datos en datos.txt');
-        //     } else {
-        //         throw err;
-        //     }
-        // });
-        
-        req = http.request(optionsHTTP, res => {
-            console.log(`\nHTTP: Status: ${res.statusCode} ${res.statusMessage}`)
-             
-            res.on('data', d => {
-                console.log(`\nAWS responde desde ${optionsHTTP.path}`)
-                process.stdout.write(d)
-            })
-        })
-          
-        req.on('error', error => {
-        console.error(error)
-        })
-        
-        req.write(JSON.stringify(data))
-        req.end()
-    })
-
-    client.on('connect', ()=>{
-        client.subscribe(topic1);
-        client.subscribe(topic2);
-        client.subscribe(topic3);
-        client.subscribe(topic4);
-        client.subscribe(topic5);
-    })
+    messageMQTT();
+    connectMQTT();
 })
